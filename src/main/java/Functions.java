f8986bf89c28a99ab0f9abf5b8b345ab4efe7143
justbegginer <blackcloudsinthesky@protonmail.com>
2022-03-12 17:35:02 +0300

    Add sort by name function

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -1,4 +1,4 @@
-import java.*;
+//import java.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class Functions {
 
-    public static List<String> isSentenceRight(ArrayList<String> arrayList){ // the sentence is correct
+    public static List<String> isSentenceRight(List<String> arrayList){ // the sentence is correct
         // if his first chart is capital, and ends on . ! ?
         Predicate<String> predicate = (str) ->
         {
@@ -21,10 +21,47 @@ public class Functions {
                 .collect(Collectors.toList());
     }
 
-    public static List<Integer> sortByBiggestAbsoluteValue(ArrayList<Integer> arrayList){
+    public static List<Integer> sortByBiggestAbsoluteValue(List<Integer> arrayList){
         Comparator<Integer> comparator = (left, right) ->
+                Math.abs(left) - Math.abs(right);
+        return arrayList.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+    public static List<List<String>> sortByName(List<List<String>> arrayList){//lexicographic sorting of names by first name and
+        //after this second
+        Comparator<List<String>> comparator = (left, right) ->
         {
-            return Math.abs(right) - Math.abs(left);
+            for (int i = 0; i < left.get(0).length() && i < right.get(0).length(); i++){
+                if (left.get(0).charAt(i) > right.get(0).charAt(i)){
+                    return 1;
+                }
+                else if(left.get(0).charAt(i) < right.get(0).charAt(i)){
+                    return -1;
+                }
+            }
+            if (left.get(0).length() > right.get(0).length()){
+                return -1;
+            }
+            else if(left.get(0).length() < right.get(0).length()){
+                return 1;
+            }
+            for (int i = 0; i < left.get(1).length() && i < right.get(1).length(); i++){
+                if (left.get(1).charAt(i) > right.get(1).charAt(i)){
+                    return 1;
+                }
+                else if(left.get(1).charAt(i) < right.get(1).charAt(i)){
+                    return -1;
+                }
+            }
+            if (left.get(1).length() > right.get(1).length()){
+                return -1;
+            }
+            else if(left.get(1).length() < right.get(1).length()){
+                return 1;
+            }
+            return 0;
         };
         return arrayList.stream()
                 .sorted(comparator)
