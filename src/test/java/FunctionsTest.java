3b6cd6fb62eb02a54e78787c9f3a6a34a6b0fb80
justbegginer <blackcloudsinthesky@protonmail.com>
2022-03-29 04:58:27 +0300

    add test for getListOfNumberBy and makeArrayFromMatrix functions

--- src/test/java/FunctionsTest.java
+++ src/test/java/FunctionsTest.java
@@ -278,4 +278,16 @@ class FunctionsTest {
         assertEquals(Functions.listOfStringToInt(Arrays.asList("1", "2", "10")), Arrays.asList(1, 2, 10));
         assertEquals(Functions.listOfStringToInt(Arrays.asList("123", "234a", "a")), Arrays.asList(123));
     }
+
+    @Test
+    void getListOfNumberBy(){
+        assertEquals(Functions.getListOfNumberBy(1, 10, 2), Arrays.asList(1, 3, 5, 7, 9));
+        assertEquals(Functions.getListOfNumberBy(2, 14, 3), Arrays.asList(2, 5, 8, 11, 14));
+    }
+
+    @Test
+    void makeArrayFromMatrix(){
+        List<List<Integer>> list = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9));
+        assertEquals(Functions.makeArrayFromMatrix(list), Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    }
 }
